1f59a4c5b230c018569bc8ebf4b6ba7cf224d52f
pschilakantitech <31645313+pschilakantitech@users.noreply.github.com>
2018-08-20 21:53:02 +0530

    Create system_up.sh
    init commit on sys up file

---
+++ system_up.sh
@@ -0,0 +1,11 @@
+sudo apt install -y libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+sudo dpkg -i google-chrome*.deb 
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt update
+sudo apt install -y sublime-text-installer
+
+sudo apt install -y git
