da9c96320e045ba026406309774f3257218f4d0a
pschilakantitech <31645313+pschilakantitech@users.noreply.github.com>
2018-08-20 21:54:41 +0530

    Create safex_mine.sh
    safex mining start file

---
+++ safex_mine.sh
@@ -0,0 +1,6 @@
+sudo apt update 
+sudo apt install -y build-essential cmake pkg-config \
+    libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libminiupnpc-dev \
+    libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev \
+    doxygen graphviz libpcsclite-dev
+sudo apt update
